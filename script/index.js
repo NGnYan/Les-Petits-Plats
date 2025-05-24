1d0da3bd022a1423a81486ed042b1e2e0313e5c0
NGnYan <ngnyan@air-de-ngnyan.home>
2025-05-25 01:29:47 +0200

    create cards

--- script/index.js
+++ script/index.js
@@ -17,3 +17,43 @@ async function getRecipes() {
 }
 
 // Filters
+
+// Cards
+async function displayRecipes() {
+  const recipes = await getRecipes();
+
+  recipes.forEach((recipe) => {
+    const card = document.createElement("div");
+    card.className = "card bg-white h-[450px] w-[300px] rounded-lg mb-[50px]";
+
+    const img = document.createElement("img");
+    img.src = recipe.image;
+    img.alt = recipe.name;
+
+    const title = document.createElement("h2");
+    title.textContent = recipe.name;
+
+    const subtitleRecipe = document.createElement("h3");
+    subtitleRecipe.textContent = "RECETTE";
+
+    const description = document.createElement("p");
+    description.textContent = recipe.description;
+
+    const subtitleIngredient = document.createElement("h3");
+    subtitleIngredient.textContent = "INGRÉDIENTS";
+
+    const ingredient = document.createElement("p");
+    ingredient.textContent = recipe.ingredients;
+
+    card.appendChild(img);
+    card.appendChild(title);
+    card.appendChild(subtitleRecipe);
+    card.appendChild(description);
+    card.appendChild(subtitleIngredient);
+    card.appendChild(ingredient);
+
+    cardContainer.appendChild(card);
+  });
+}
+
+displayRecipes();
