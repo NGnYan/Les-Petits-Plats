64734e0156f91fabf6dc1af2d3a00a8f72b96651
NGnYan <ngnyan@air-de-ngnyan.home>
2025-05-28 16:58:42 +0200

    update cards

--- script/index.js
+++ script/index.js
@@ -1,5 +1,9 @@
 import "../css/style.css";
 
+// Class
+
+const subtitleClassCard = ["text-[#ababab]", "pl-[30px]", "pt-[40px]"];
+
 // Selectors
 const cardContainer = document.querySelector(".card-container");
 const numberRecipes = document.querySelector(".number-recipes");
@@ -40,7 +44,7 @@ async function displayRecipes() {
   recipes.forEach((recipe) => {
     const card = document.createElement("div");
     card.className =
-      "card bg-white h-[700px] w-full rounded-xl mb-[50px] mx-auto overflow-hidden";
+      "card relative bg-white h-full w-full rounded-2xl mb-[50px] mx-auto overflow-hidden";
 
     const imgRecipe = document.createElement("img");
     imgRecipe.src = `./assets/recipes/${recipe.image}`;
@@ -58,30 +62,81 @@ async function displayRecipes() {
     titleRecipe.classList.add(
       "font-anton",
       "font-bold",
-      "text-[1.2em]",
+      "text-[0.9em]",
       "pl-[30px]",
-      "pt-[40px]",
-      "pb-[40px]"
+      "pt-[40px]"
     );
 
     const subtitleRecipe = document.createElement("h3");
     subtitleRecipe.textContent = "RECETTE";
+    subtitleRecipe.classList.add(...subtitleClassCard);
 
     const description = document.createElement("p");
     description.textContent = recipe.description;
+    description.classList.add(
+      "text-[0.9em]",
+      "pl-[30px]",
+      "pt-[20px]",
+      "pr-[30px]"
+    );
 
     const subtitleIngredient = document.createElement("h3");
     subtitleIngredient.textContent = "INGRÉDIENTS";
+    subtitleIngredient.classList.add(...subtitleClassCard);
 
-    const ingredient = document.createElement("p");
-    ingredient.textContent = recipe.ingredients.join(", ");
+    const ingredients = document.createElement("ul");
+    ingredients.classList.add(
+      "grid",
+      "grid-cols-2",
+      "gap-x-[50px]",
+      "gap-y-[20px]",
+      "text-sm",
+      "ml-[30px]",
+      "mr-[30px]"
+    );
+
+    // Display ingredient, quantity and unit in the card
+    recipe.ingredients.forEach((ingredient) => {
+      const li = document.createElement("li");
+      li.classList.add("flex", "flex-col", "gap-[5px]", "pt-[20px]");
+
+      const ingredientName = document.createElement("span");
+      ingredientName.textContent = ingredient.ingredient;
+
+      const ingredientQuantity = document.createElement("span");
+      if (ingredient.quantity) {
+        ingredientQuantity.textContent = ` ${ingredient.quantity}`;
+        if (ingredient.unit) {
+          ingredientQuantity.textContent += ` ${ingredient.unit}`;
+        }
+        ingredientQuantity.classList.add("text-[#ababab]");
+      }
+
+      li.appendChild(ingredientName);
+      li.appendChild(ingredientQuantity);
+      ingredients.appendChild(li);
+    });
+
+    const boxTime = document.createElement("div");
+    boxTime.textContent = `${recipe.time} min`;
+    boxTime.classList.add(
+      "bg-[#FFD15B]",
+      "absolute",
+      "w-200px",
+      "py-[5px]",
+      "px-[15px]",
+      "rounded-[100px]",
+      "top-2",
+      "right-2"
+    );
 
     card.appendChild(imgRecipe);
     card.appendChild(titleRecipe);
     card.appendChild(subtitleRecipe);
     card.appendChild(description);
     card.appendChild(subtitleIngredient);
-    card.appendChild(ingredient);
+    card.appendChild(ingredients);
+    card.appendChild(boxTime);
 
     cardContainer.appendChild(card);
   });
