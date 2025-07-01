38eab28887c840e20f284cf2e9e001bdda776a50
NGnYan <ngnyan@air-de-ngnyan.home>
2025-07-01 22:37:02 +0200

    fix input-dropdown correct selection and display issues

--- script/components/dropdownList.js
+++ script/components/dropdownList.js
@@ -5,6 +5,72 @@ export function displayDropdown(id, recipes, category) {
   const dropdown = document.getElementById(id);
   dropdown.innerHTML = "";
 
+  const searchContainer = document.createElement("div");
+  searchContainer.className = `
+  relative
+  w-full
+  mb-2
+`;
+
+  const inputDropdown = document.createElement("input");
+  inputDropdown.type = "text";
+  inputDropdown.placeholder = "Rechercher";
+  inputDropdown.className = `
+  w-full
+  pl-3 pr-10
+  py-2
+  border
+  border-gray-300
+  rounded
+  text-sm
+  focus:outline-none
+  box-border
+`;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+  deleteBtn.className = `
+  absolute
+  right-8
+  top-1/2
+  -translate-y-1/2
+  text-gray-500
+  hover:text-black
+  text-sm
+  cursor-pointer
+  hidden
+`;
+
+  deleteBtn.addEventListener("click", () => {
+    inputDropdown.value = "";
+    deleteBtn.classList.add("hidden");
+    inputDropdown.focus();
+  });
+
+  const searchIcon = document.createElement("span");
+  searchIcon.innerHTML = `<i class="fa-solid fa-magnifying-glass"></i>`;
+  searchIcon.className = `
+  absolute
+  right-2
+  top-1/2
+  -translate-y-1/2
+  text-gray-400
+ cursor-pointer
+`;
+
+  searchContainer.appendChild(inputDropdown);
+  searchContainer.appendChild(deleteBtn);
+  searchContainer.appendChild(searchIcon);
+  dropdown.appendChild(searchContainer);
+
+  inputDropdown.addEventListener("input", () => {
+    if (inputDropdown.value.length > 0) {
+      deleteBtn.classList.remove("hidden");
+    } else {
+      deleteBtn.classList.add("hidden");
+    }
+  });
+
   recipes
     .sort((a, b) => a.localeCompare(b, "fr", { sensitivity: "base" }))
     .forEach((elmt) => {
@@ -15,10 +81,13 @@ export function displayDropdown(id, recipes, category) {
     cursor-pointer
     mb-1
     py-2
-    hover:bg-gray-100
+    px-4
+    hover:bg-[#FFD15B]
     text-gray-700
     rounded
-    mb-1
+    block
+    w-full
+    box-border
   `;
 
       li.addEventListener("click", () => {
